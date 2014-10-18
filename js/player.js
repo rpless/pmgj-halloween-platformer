cfe3b6cf14f144fae9a3028d81cbe0f2e7aba889
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-18 15:32:13 -0400

    added floors and 'colors' to blocks

--- js/player.js
+++ js/player.js
@@ -1,11 +1,27 @@
 define(['crafty'], function(Crafty) {
 
+  Crafty.c('Keyboard', {
+    isDown: function(key) {
+      return typeof key === 'string' && !!Crafty.keydown[Crafty.keys[key]];
+    }
+  });
+
   return {
     create: function() {
-      var player = Crafty.e('2D, Canvas');
-      player.addComponent('Color').color('#F00');
+      var player = Crafty.e('2D, Canvas, Color, Collison');
       player.addComponent('Fourway').fourway(5);
       player.addComponent('Gravity').gravity('Blue');
+      player.bind('KeyDown', function(e) {
+        if (e.key === Crafty.keys.I) {
+          this.antigravity(['Red', 'Blue']);
+          this.color('#0F0');
+          this.gravity('Green');
+        } else if (e.key === Crafty.keys.J) {
+          this.antigravity(['Red', 'Green']);
+          this.color('#00F');
+          this.gravity('Blue');
+        }
+      });
 
       return player;
     }
