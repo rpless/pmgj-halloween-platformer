24713aea97d79cecc34ee3a0401891638828da84
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-18 19:09:16 -0400

    passed the block type in from the loader

--- js/levelLoader.js
+++ js/levelLoader.js
@@ -7,7 +7,7 @@ define(['require', 'block'], function(require, block) {
           var platform = platforms[i];
           var x = platform.x - (platform.w / 2);
           var y = platform.y - (platform.h / 2);
-          var b = block.create().attr({ x: x, y: y, w: platform.w, h: platform.h });
+          var b = block.create(platform.costume).attr({ x: x, y: y, w: platform.w, h: platform.h });
           b.addComponent(platform.costume);
         }
       }
