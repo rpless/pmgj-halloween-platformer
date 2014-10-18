76ea71298b496a222bee7fd861b66ce18031cb9f
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-18 13:26:01 -0400

    added styles to the game

--- js/platformer.js
+++ js/platformer.js
@@ -1,3 +1,4 @@
 define(['crafty', 'scenes/platformerScene'], function(Crafty, platformerScene) {
+  Crafty.init(800, 600, document.getElementById('game'));
   platformerScene.start();
 });
--- js/scenes/platformerScene.js
+++ js/scenes/platformerScene.js
@@ -1,6 +1,5 @@
 define(['crafty'], function(Crafty) {
   Crafty.defineScene('Platformer', function() {
-    Crafty.init(800,600, document.getElementById('game'));
     Crafty.e('2D, DOM, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#F00');
   });
 
