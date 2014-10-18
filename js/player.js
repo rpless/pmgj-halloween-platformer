9228ab57fb7b64b43714bd8bd713270e20911b73
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-18 16:44:53 -0400

    fixed collision

--- js/player.js
+++ js/player.js
@@ -10,7 +10,7 @@ define(['crafty', 'costume'], function(Crafty) {
     create: function(type) {
       var player = Crafty.e('2D, Canvas, Color, Collison, ' + type);
       player.addComponent('Fourway').fourway(5);
-      player.addComponent('Gravity').gravity('Blue');
+      player.addComponent('Gravity').gravity('Ghost');
       player.bind('KeyDown', function(e) {
         if (e.key === Crafty.keys.I) {
           this.changeCostume('Pumpkin');
