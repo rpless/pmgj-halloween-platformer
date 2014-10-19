c8bd31bf9f2d66b88feeca1865127dd557c21f4a
Brad Whorton <Bnwhorton@gmail.com>
2014-10-19 16:14:52 -0400

    Added levels 8-10

---
+++ js/levels/level8.js
@@ -0,0 +1,75 @@
+define(function() {
+  return {
+    "platforms": [
+      {
+        "costume": "Pumpkin",
+        "x": 1120,
+        "y": 288,
+        "w": 256,
+        "h": 64
+      },
+      {
+        "costume": "Pumpkin",
+        "x": 160,
+        "y": 928,
+        "w": 320,
+        "h": 64,
+	"e": 1
+      },
+      {
+        "costume": "Ghost",
+        "x": 1728,
+        "y": 480,
+        "w": 384,
+        "h": 64
+      },
+      {
+        "costume": "Pumpkin",
+        "x": 768,
+        "y": 288,
+        "w": 256,
+        "h": 64
+      },
+      {
+        "costume": "Spider",
+        "x": 352,
+        "y": 288,
+        "w": 256,
+        "h": 64
+      },
+      {
+        "costume": "Ghost",
+        "x": 1440,
+        "y": 288,
+        "w": 256,
+        "h": 64
+      },
+      {
+        "costume": "Spider",
+        "x": 1344,
+        "y": 608,
+        "w": 256,
+        "h": 64
+      },
+      {
+        "costume": "Spider",
+        "x": 960,
+        "y": 704,
+        "w": 256,
+        "h": 64
+      },
+      {
+        "costume": "Pumpkin",
+        "x": 576,
+        "y": 800,
+        "w": 256,
+        "h": 64
+      },
+    ],
+    "enemies": [
+      { "x": 50, "y": 50, "costume": "Spider" }
+    ],
+    "start": { "x": 352, "y": 224},
+    "end": { "x": 64, "y": 864}
+  };
+});
---
+++ js/levels/level9.js
@@ -0,0 +1,49 @@
+define(function() {
+  return {
+    "platforms": [
+      {
+        "costume": "Pumpkin",
+        "x": 800,
+        "y": 768,
+        "w": 1600,
+        "h": 64
+      },
+      {
+        "costume": "Spider",
+        "x": 512,
+        "y": 608,
+        "w": 320,
+        "h": 64,
+	"e": 1
+      },
+      {
+        "costume": "Ghost",
+        "x": 1664,
+        "y": 448,
+        "w": 512,
+        "h": 64
+      },
+      {
+        "costume": "Spider",
+        "x": 832,
+        "y": 608,
+        "w": 256,
+        "h": 64,
+	"e": 1
+      },
+      {
+        "costume": "Spider",
+        "x": 1152,
+        "y": 608,
+        "w": 256,
+        "h": 64,
+	"e": 1
+      },
+    ],
+    "enemies": [
+      { "x": 50, "y": 50, "costume": "Spider" }
+    ],
+    "start": { "x": 96, "y": 704},
+    "end": { "x": 1792, "y": 384}
+  };
+});
