b3b44e96ded077b4fcdf744efa9a79f79dd5b61e
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-19 09:06:44 -0400

    fixed most of the y portion of physics

--- js/costume.js
+++ js/costume.js
@@ -7,8 +7,24 @@ define(['crafty'], function(Crafty) {
       init: function() {
         this.requires('2D, Canvas, Color');
         this.color(color);
+      },
+
+      costume: function(type) {
+        if (type) {
+          this._costume = type;
+        } else {
+          return this._costume;
+        }
+      },
+
+      weak: function(type) {
+        if (type) {
+          this._weak = type;
+        } else {
+          return this._weak;
+        }
       }
-    },weak);
+    });
   };
 
   createCostume('Spider', '#F00','Ghost');
--- js/player.js
+++ js/player.js
@@ -1,46 +1,80 @@
 define(['crafty', 'costume'], function(Crafty) {
 
-  Crafty.c('Keyboard', {
-    isDown: function(key) {
-      return typeof key === 'string' && !!Crafty.keydown[Crafty.keys[key]];
+  Crafty.c('Player', {
+    _grav: 0,
+    _gravConst: 0.2,
+
+    init: function() {
+      this.requires('2D, Canvas, Color, Collision, Ghost, PlayerCostume, Fourway');
+      this.fourway(10);
+      this.bind('EnterFrame', this._enterFrame);
+      this.onHit('Platform', function(hits) {
+        this._adjustForCollision(hits, 'y', 'h');
+      });
+      this.onHit('Candy', this._candyCollide);
+    },
+
+    _enterFrame: function() {
+      this._grav += 0.2;
+      this.y += this._grav;
+    },
+
+    _adjustForCollision: function(hits, dir, size) {
+      var largestOverlap = 0;
+      var newValue = this[dir];
+      for (var i = 0; i < hits.length; i++) {
+        var hitObj = hits[i].obj;
+        if (this.costume() === hitObj.costume()) {
+          var playerBottom = this[dir] + this[size];
+          var platformBottom = hitObj[dir] + hitObj[size];
+          if (hitObj[dir] < playerBottom && playerBottom < platformBottom && largestOverlap < playerBottom - hitObj[dir]) {
+            largestOverlap = Math.abs(playerBottom - hitObj[dir]);
+            newValue = (hitObj[dir] - largestOverlap) - this[size];
+          } else if (this[dir] < platformBottom && platformBottom < playerBottom && largestOverlap < platformBottom - this[dir]) {
+            largestOverlap = Math.abs(platformBottom - this[dir]);
+            newValue = (this[dir] + largestOverlap);
+          }
+        }
+      }
+      if (this[dir] !== newValue) this._grav = 0;
+      this[dir] = newValue;
+    },
+
+    _candyCollide: function(hit) {
+      console.log('Got the Candy!');
+      for (var i = 0; i < hit.length; i++) {
+        var obj = hit[i].obj;
+        if (obj.has('Candy')) {
+          obj.destroy();
+        }
+      }
     }
   });
 
   return {
     create: function(type) {
-      var player = Crafty.e('2D, Canvas, Color, Collision, ' + type);
-      player.addComponent('Fourway').fourway(10);
-      player.addComponent('Gravity').gravity('Ghost');
-      player.bind('KeyDown', function(e) {
-        if (e.key === Crafty.keys.I) {
-          this.changeCostume('Pumpkin');
-          this.antigravity(['Spider', 'Ghost']);
-          this.gravity('Pumpkin');
-        } else if (e.key === Crafty.keys.J) {
-          this.changeCostume('Ghost');
-          this.antigravity(['Spider', 'Pumpkin']);
-          this.gravity('Ghost');
-        } else if (e.key === Crafty.keys.L) {
-          this.changeCostume('Spider');
-          this.antigravity(['Ghost', 'Pumpkin']);
-          this.gravity('Spider');
-        }
-      });
+      var player = Crafty.e('Player');
+      // player.addComponent('Gravity');
+      // player.bind('KeyDown', function(e) {
+      //   if (e.key === Crafty.keys.I) {
+      //     this.changeCostume('Pumpkin');
+      //     this.antigravity(['Spider', 'Ghost']);
+      //     this.gravity('Pumpkin');
+      //   } else if (e.key === Crafty.keys.J) {
+      //     this.changeCostume('Ghost');
+      //     this.antigravity(['Spider', 'Pumpkin']);
+      //     this.gravity('Ghost');
+      //   } else if (e.key === Crafty.keys.L) {
+      //     this.changeCostume('Spider');
+      //     this.antigravity(['Ghost', 'Pumpkin']);
+      //     this.gravity('Spider');
+      //   }
+      // });
+
       player.onHit('Enemy', function(hit) {
         if (hit[0].obj.has(this._cweak)) {
 		    }
 	    });
-      player.addComponent('PlayerCostume');
-
-      player.onHit('Candy', function(hit) {
-        console.log('Got the Candy!');
-        for (var i = 0; i < hit.length; i++) {
-          var obj = hit[i].obj;
-          if (obj.has('Candy')) {
-            obj.destroy();
-          }
-        }
-      });
 
       return player;
     }
