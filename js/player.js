8df35b795c4935fe902cad4fb55fecb2e88b869f
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-19 13:59:23 -0400

    fixed music and jumps again

--- js/player.js
+++ js/player.js
@@ -2,8 +2,9 @@ define(['crafty', 'costume', 'Scroller'], function(Crafty) {
 
   Crafty.c('Player', {
     _grav: 0,
-    _gravConst: 0.2,
-    _speed: 10,
+    _gravConst: 0.4,
+    _speed: 8,
+    _jump: 5,
     _canJump: true,
 
     init: function() {
@@ -26,10 +27,6 @@ define(['crafty', 'costume', 'Scroller'], function(Crafty) {
         if (this._didHit()) {
           this.x -= this._speed;
         }
-      }
-      if (this.isDown('W') && this._canJump) {
-        this._grav -= this._speed;
-        this._canJump = false;
       }
       this._grav += this._gravConst;
       if (!this._didHit()) {
@@ -54,7 +51,7 @@ define(['crafty', 'costume', 'Scroller'], function(Crafty) {
       }
       if (this.isDown('W') && this._canJump) {
         this._canJump = false;
-        this._grav -= (this._speed / 2);
+        this._grav -= this._jump;
       }
     },
 
