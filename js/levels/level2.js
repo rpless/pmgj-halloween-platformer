6c0851d8b97b7819297875075547f9fc2c6748fd
Brad Whorton <Bnwhorton@gmail.com>
2014-10-19 10:48:43 -0400

    Add level2.js

---
+++ js/levels/level2.js
@@ -0,0 +1,67 @@
+define(function() {
+  return {
+    "platforms": [
+      {
+        "costume": "Pumpkin",
+        "x": 160,
+        "y": 896,
+        "w": 320,
+        "h": 64
+      },
+      {
+        "costume": "Pumpkin",
+        "x": 544,
+        "y": 768,
+        "w": 288,
+        "h": 64
+      },
+      {
+        "costume": "Pumpkin",
+        "x": 240,
+        "y": 544,
+        "w": 160,
+        "h": 64
+      },
+      {
+        "costume": "Pumpkin",
+        "x": 496,
+        "y": 352,
+        "w": 160,
+        "h": 64
+      },
+      {
+        "costume": "Ghost",
+        "x": 1184,
+        "y": 352,
+        "w": 192,
+        "h": 64
+      },
+      {
+        "costume": "Ghost",
+        "x": 1824,
+        "y": 192,
+        "w": 192,
+        "h": 64
+      },
+      {
+        "costume": "Ghost",
+        "x": 832,
+        "y": 224,
+        "w": 192,
+        "h": 64
+      },
+      {
+        "costume": "Pumpkin",
+        "x": 1552,
+        "y": 352,
+        "w": 256,
+        "h": 64
+      },
+    ],
+    "enemies": [
+      { "x": 50, "y": 50, "costume": "Spider" }
+    ],
+    "start": { "x": 64, "y": 832},
+    "end": { "x": 1824, "y": 96}
+  };
+});
