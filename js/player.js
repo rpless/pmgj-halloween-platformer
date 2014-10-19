8825e47fbbf89c376d43c048e11ba8a8a223b465
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-19 09:43:16 -0400

    physics! it almost works

--- js/player.js
+++ js/player.js
@@ -3,41 +3,80 @@ define(['crafty', 'costume'], function(Crafty) {
   Crafty.c('Player', {
     _grav: 0,
     _gravConst: 0.2,
+    _speed: 10,
+    _canJump: true,
 
     init: function() {
-      this.requires('2D, Canvas, Color, Collision, Ghost, PlayerCostume, Fourway');
-      this.fourway(10);
+      this.requires('2D, Canvas, Color, Collision, Ghost, PlayerCostume, Keyboard');
       this.bind('EnterFrame', this._enterFrame);
-      this.onHit('Platform', function(hits) {
-        this._adjustForCollision(hits, 'y', 'h');
-      });
       this.onHit('Candy', this._candyCollide);
     },
 
     _enterFrame: function() {
-      this._grav += 0.2;
-      this.y += this._grav;
+      if (this.isDown('A')) {
+        this.x -= this._speed;
+        if (this.hit('Platform')) {
+          this.x += this._speed;
+        }
+      }
+      if (this.isDown('D')) {
+        this.x += this._speed;
+        if (this.hit('Platform')) {
+          this.x -= this._speed;
+        }
+      }
+      if (this.isDown('W') && this._canJump) {
+        this._grav -= this._speed;
+        this._canJump = false;
+      }
+      this._grav += this._gravConst;
+      if (!this.hit('Platform')) {
+        this.y += this._grav;
+        var resetGrav = false;
+        while (this.hit('Platform')) {
+          resetGrav = true;
+          if (this._grav > 0) {
+            this._canJump = true;
+            this.y -= this._gravConst;
+          } else if (this._grav < 0) {
+            this._canJump = false;
+            this.y += this._gravConst;
+          }
+        }
+        if (resetGrav) {
+          this._grav = 0;
+          resetGrav = false;
+        } else {
+          this._canjump = false;
+        }
+      }
+      if (this.isDown('W') && this._canJump) {
+        this._canJump = false;
+        this._grav -= (this._speed / 2);
+      }
     },
 
     _adjustForCollision: function(hits, dir, size) {
       var largestOverlap = 0;
-      var newValue = this[dir];
+      var newValue = this.y;
       for (var i = 0; i < hits.length; i++) {
         var hitObj = hits[i].obj;
         if (this.costume() === hitObj.costume()) {
-          var playerBottom = this[dir] + this[size];
-          var platformBottom = hitObj[dir] + hitObj[size];
-          if (hitObj[dir] < playerBottom && playerBottom < platformBottom && largestOverlap < playerBottom - hitObj[dir]) {
-            largestOverlap = Math.abs(playerBottom - hitObj[dir]);
-            newValue = (hitObj[dir] - largestOverlap) - this[size];
-          } else if (this[dir] < platformBottom && platformBottom < playerBottom && largestOverlap < platformBottom - this[dir]) {
-            largestOverlap = Math.abs(platformBottom - this[dir]);
-            newValue = (this[dir] + largestOverlap);
+          var playerBottom = this.y + this.h;
+          var platformBottom = hitObj.y + hitObj.h;
+          if (hitObj.y < playerBottom && playerBottom < platformBottom && largestOverlap < playerBottom - hitObj.y) {
+            largestOverlap = Math.abs(playerBottom - hitObj.y);
+            newValue = (hitObj.y - largestOverlap) - this.h;
+            this._canJump = false;
+          } else if (this.y < platformBottom && platformBottom < playerBottom && largestOverlap < platformBottom - this.y) {
+            largestOverlap = Math.abs(platformBottom - this.y);
+            newValue = (this.y + largestOverlap);
+
           }
         }
       }
-      if (this[dir] !== newValue) this._grav = 0;
-      this[dir] = newValue;
+      if (this.y !== newValue) this._grav = 0;
+      this.y = newValue;
     },
 
     _candyCollide: function(hit) {
